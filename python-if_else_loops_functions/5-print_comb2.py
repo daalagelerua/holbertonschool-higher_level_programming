88ecce48e4041597e391c672e39d097de07c6776
daalagelerua <aurele284@gmail.com>
2025-01-14 15:16:05 +0100

    spaces for indentation pycodestyle

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-#version compact: print(", ".join(f"{i:02}" for i in range(100)))
-for i in range (100):
-	if i < 99:
-		print(f"{i:02}", end=", ")
-	else:
-		print(i)
+# version compact: print(", ".join(f"{i:02}" for i in range(100)))
+for i in range(100):
+    if i < 99:
+        print(f"{i:02}", end=", ")
+    else:
+        print(i)
