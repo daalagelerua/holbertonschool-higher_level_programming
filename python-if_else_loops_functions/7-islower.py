ad2fc2caa38bc287dd8ac928eea4e8781fbcc1aa
daalagelerua <aurele284@gmail.com>
2025-01-14 17:25:37 +0100

    islower

---
+++ python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# version compacte:  return ord(c) >= 97 and ord(c) <= 122
+def islower(c):
+
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
