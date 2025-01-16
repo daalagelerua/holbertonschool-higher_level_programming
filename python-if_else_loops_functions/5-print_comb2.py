002363dfeb7adc455164913371c4fe6ef5fd1e5a
daalagelerua <aurele284@gmail.com>
2025-01-16 14:16:51 +0100

    same

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-# version compact: print(", ".join(f"{i:02}" for i in range(100)))
-for i in range(100):
-    if i < 99:
+for i in range(0, 100):
+    if i != 99:
         print("{:02d}".format(i), end=", ")
     else:
         print("{:02d)".format(i))
