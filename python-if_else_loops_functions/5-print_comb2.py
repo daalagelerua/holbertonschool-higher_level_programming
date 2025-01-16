36c6e6c52becc773b337834f73db999913781b77
daalagelerua <aurele284@gmail.com>
2025-01-16 14:10:04 +0100

    5-print_comb2.py changement.format

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -2,6 +2,6 @@
 # version compact: print(", ".join(f"{i:02}" for i in range(100)))
 for i in range(100):
     if i < 99:
-        print(f"{i:02}", end=", ")
+        print("{:02}".format(i), end=", ")
     else:
         print(i)
