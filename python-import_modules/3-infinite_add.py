ccbc51a8fb354f72bc2e87e24ea637cb4f37fb29
daalagelerua <aurele284@gmail.com>
2025-01-16 16:02:42 +0100

    no exec

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 import sys
 
-sum = 0
+if __name__ == "__main__":
 
-for i in sys.argv[1:]:
-    sum += int(i)
-print(sum)
+    sum = 0
+
+    for i in sys.argv[1:]:
+        sum += int(i)
+    print(sum)
