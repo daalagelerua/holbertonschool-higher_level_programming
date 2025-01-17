15a20e751d098fac4698a244d611b180b6553070
Aurèle Perillat <aurele284@gmail.com>
2025-01-17 19:49:06 +0100

    Delete python-if_else_loops_functions/pld directory

--- python-if_else_loops_functions/pld/0-add.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-from add_0 import add
-
-a = add(1, 2)
-print(f"1 + 2 = {a}")
--- python-if_else_loops_functions/pld/3-infinite_add.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-import sys
-
-sum = 0
-
-for i in sys.argv[1:]:
-    sum += int(i)
-print(sum)
