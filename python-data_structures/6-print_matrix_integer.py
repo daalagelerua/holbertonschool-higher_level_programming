53fb3b8562848573816f76550e804a0197a84b0b
daalagelerua <aurele284@gmail.com>
2025-01-20 16:35:30 +0100

    print a matrix

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+
+    for row in range(len(matrix)):
+        for column in range(len(matrix[row])):
+            if column == len(matrix[row]) - 1:  #if last element of row
+                print("{:d}".format(matrix[row][column]), end='')
+            else:
+                print("{:d}".format(matrix[row][column]), end=' ')
+        print()
