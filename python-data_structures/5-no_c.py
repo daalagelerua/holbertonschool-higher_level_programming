bb60687b53ecf99e530c3a55e2dca092ca182b22
daalagelerua <aurele284@gmail.com>
2025-01-21 11:16:41 +0100

    changement syntax

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
 
-    new_string = [item for item in my_string if item != 'c' and item != 'C']
-    return (' '.join(new_string))
+    return (''.join(item for item in my_string if item not in 'cC'))
