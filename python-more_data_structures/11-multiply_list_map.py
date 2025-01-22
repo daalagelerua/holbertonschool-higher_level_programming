af04a218bc2c470fad5a9e312d204e9c14b2df54
daalagelerua <aurele284@gmail.com>
2025-01-22 13:42:14 +0100

    delete comment on 11 + whiteboarding 12

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-'''
-map apply function for each element of iterable & return iterator
-x represent each element of my_list
-list() convert iterator in a list
-'''
     return list(map(lambda x: x * number, my_list))
---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+
+    if not roman_string or roman_string is None:
+        return 0
+
+    X = 10
+    I = 1
+    V = 5
+    L = 50
+    C = 100
+    D = 500
+    M = 1000
+
+    total = 0
+    for element in roman_string:
+        
+
+
+    return total
