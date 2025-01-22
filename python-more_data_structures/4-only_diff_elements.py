9918e6f3b46ec141da285f9d83f90094e7c43a61
daalagelerua <aurele284@gmail.com>
2025-01-22 10:53:08 +0100

    uncommon elements of a list

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
 
-    
+    uncommon_set = set()
+
+    # Iterate over each element in the first set
+    for element in set_1:
+        # Check if the element is not in the second set
+        if element not in set_2:
+            # If it's not, add it to the uncommon set
+            uncommon_set.add(element)
+    # repeat in reverse
+    for element in set_2:
+        if element not in set_1:
+            uncommon_set.add(element)
+
+    # Return the set of uncommon elements
+    return uncommon_set
