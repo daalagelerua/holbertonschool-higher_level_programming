f0da8395b097667a5feb9a7b0ccb1a262ae90155
daalagelerua <aurele284@gmail.com>
2025-01-22 10:42:12 +0100

    common element in set

---
+++ python-more_data_structures/3-common_elements.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+
+    common_set = ()
+
+    for element in set_1:
+        # Check if the element is also in the second set
+        if element in set_2:
+            # If yes, add it to the common set
+            common_set.add(element)
+
+    return common_set
