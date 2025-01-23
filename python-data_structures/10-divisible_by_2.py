58f5cec5f5ebc6570492113ed7f33382e3777537
daalagelerua <aurele284@gmail.com>
2025-01-23 10:30:54 +0100

    changing variable name

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -3,8 +3,8 @@ def divisible_by_2(my_list=[]):
     # create a new list to store true or false for each element
     result = []
 
-    for l in my_list:
-        if l % 2 == 0:
+    for element in my_list:
+        if element % 2 == 0:
             result.append(True)
         else:
             result.append(False)
