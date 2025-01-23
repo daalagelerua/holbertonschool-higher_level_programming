7347eaebe75431d0da14c6d8f194324c1b3df5d6
daalagelerua <aurele284@gmail.com>
2025-01-23 10:09:07 +0100

    fixing comment

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
 
     for row in range(len(matrix)):
         for column in range(len(matrix[row])):
-            if column == len(matrix[row]) - 1:  #if last element of row
+            if column == len(matrix[row]) - 1:  # if last element of row
                 print("{:d}".format(matrix[row][column]), end='')
             else:
                 print("{:d}".format(matrix[row][column]), end=' ')
