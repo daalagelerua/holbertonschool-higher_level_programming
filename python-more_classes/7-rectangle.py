02befd43869b18fee4f2e1dc973b0a9cf0565d68
daalagelerua <aurele284@gmail.com>
2025-01-31 14:35:55 +0100

    blank line

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -18,7 +18,7 @@ class Rectangle:
     """
     number_of_instances = 0
     print_symbol = '#'
-    
+
     def __init__(self, width=0, height=0):
         """
         Initializes a rectangle of optional width and height.
