54f82f7a8252cc818419ac2345f0ad498bce4573
daalagelerua <aurele284@gmail.com>
2025-02-03 16:27:20 +0100

    test my_list

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 def lookup(obj):
+    """
+    This function returns a list of available
+    attributes and method of an object
+    Args:
+        obj (): object
+
+    Returns:
+        list: list of attributes and methods
+    """
     return dir(obj)
 
 
---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This module define a class 'MyList'.
+This class inherits of 'list' and add a method to 
+print a sorted version of the list
+"""
+class MyList(list):
+    """
+    Class MyList inherits of 'list' and add print_sorted
+    Args:
+        list (int): list of integer
+    """
+    def print_sorted(self):
+        """
+        Shows the list in ascending order without modifying it
+        """
+        print(sorted(self))
