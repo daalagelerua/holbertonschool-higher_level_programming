3ed647b1fc88bffdeb4bf8d6d5fa6e4741b73014
daalagelerua <aurele284@gmail.com>
2025-02-03 17:11:20 +0100

    raise exception if 'name' is not a positive integer

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This is a class that define a function area
+"""
+class BaseGeometry:
+    """
+    This class has a function
+    """
+    def area(self):
+        """
+        This function raises an exception
+        """
+        raise Exception("area() is not implemented")
+    
---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This is a class that define a function area
+"""
+class BaseGeometry:
+    """
+    This class has a function
+    """
+    def area(self):
+        """
+        This function raises an exception
+        """
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+        """
+        This function checks if value is a positive integer
+        Args:
+            name (string): name of 
+            value (int): value
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+        
