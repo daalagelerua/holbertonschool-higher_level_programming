2e2dacefd7ae32d85d85905fc63468eb03827da6
daalagelerua <aurele284@gmail.com>
2025-02-04 10:33:54 +0100

    adding some more docstring

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,26 +1,33 @@
 #!/usr/bin/python3
 """
-This is a class that define a function area
+This module define a class with a function to check
+the area
 """
 
 
 class BaseGeometry:
     """
-    This class has a function
+    This class has a function to check the area
     """
 
     def area(self):
         """
-        This function raises an exception
+        This function raises an exception if the area
+        is not implemented
         """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         """
         This function checks if value is a positive integer
+
         Args:
             name (string): name of
             value (int): value
+        
+        Raises:
+            TypeError: If value is not integer
+            ValueError: If value is less or equal to 0
         """
         if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
