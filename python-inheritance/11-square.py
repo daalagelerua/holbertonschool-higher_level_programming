9b42f62daec715d1947e3ae72bf9c5f3a906a520
daalagelerua <aurele284@gmail.com>
2025-02-04 11:01:37 +0100

    P minuscule a python3

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,4 +1,4 @@
-#!/usr/bin/Python3
+#!/usr/bin/python3
 """
 This module defines a class that inherits from another class
 """
--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,4 +1,4 @@
-#!/usr/bin/Python3
+#!/usr/bin/python3
 """
 This module defines a function that checks an instance
 """
