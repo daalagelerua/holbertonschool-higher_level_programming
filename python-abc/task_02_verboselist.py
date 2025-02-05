5e1365b117366241402e4b7a26dc40a965ee0d9d
daalagelerua <aurele284@gmail.com>
2025-02-05 16:08:57 +0100

    super()

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
 class VerboseList(list):
-    for element in list:
-        if super().append(element):
-            print(f"Added {[element]} to the list.")
-        if super().remove(element):
-            print(f"Removed {[element]} from the list.")
-        if super().pop(element):
-            print(f"Popped {[element]} from the list.")
-    for nb in range(len(list)):
-        if super().extend(nb):
-            print(f"Extended the list with {[nb]} items.")
+    def append(self, item):
+        super().append(item)
+        print(f"Added {[item]} to the list.")
+
+    def extend(self, iter):
+        num = len(iter)
+        super().extend(iter)
+        print(f"Extended the list with {[num]} items.")
+            
+
+    def remove(self, item):
+        print(f"Removed {[item]} from the list.")
+        super().remove(item)
+
+    def pop(self, index=-1):
+        item = self[index]
+        print(f"Popped {[item]} from the list.")
+        super().pop(index)
+        
