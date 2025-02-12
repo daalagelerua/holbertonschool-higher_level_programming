b9cf11f71b4ddc75372e378fafe0be2634000d68
daalagelerua <aurele284@gmail.com>
2025-02-12 15:38:44 +0100

    class student

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -40,7 +40,7 @@ class CustomObject:
         try:
             with open(filename, "wb") as file:
                 pickle.dump(self, file)
-        except (pickle.PickleError):
+        except (OSError, pickle.PickleError):
             print(f"Serialization error")
 
     @classmethod
@@ -53,5 +53,5 @@ class CustomObject:
         try:
             with open(filename, "rb") as file:
                 return pickle.load(file)
-        except (FileNotFoundError, pickle.PickleError):
+        except (FileNotFoundError, pickle.PickleError, EOFError):
             return None
