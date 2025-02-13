fb3b023d5cf7abdd1370483d585753cd569c93cb
daalagelerua <aurele284@gmail.com>
2025-02-13 13:54:29 +0100

    pascal le tritri

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def pascal_triangle(n):
+    """
+    Returns a list of lists of integers representing Pascal's triangle of n.
+    Returns an empty list if n <= 0.
+    """
+    if n <= 0:
+        return []
+    
+    triangle = []
+    for row in range(n):
+        current_row = []
+        for col in range(row + 1):
+            if col == 0 or col == row:
+                current_row.append(1)
+            else:
+                # Calculate the value based on the previous row
+                value = triangle[row - 1][col - 1] + triangle[row - 1][col]
+                current_row.append(value)
+        triangle.append(current_row)
+    
+    return triangle
