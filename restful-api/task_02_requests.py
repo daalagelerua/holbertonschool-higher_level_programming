92bee861eca4e7cb4e54b4b985bef619d94797e9
daalagelerua <aurele284@gmail.com>
2025-02-18 12:22:32 +0100

    requests

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -5,7 +5,7 @@ import csv
 
 
 def fetch_and_print_posts():
-    res = request.get('https://jsonplaceholder.typicode.com/posts')
+    res = requests.get('https://jsonplaceholder.typicode.com/posts')
 
     print(f"Status Code: {res.status_code}")
 
@@ -16,9 +16,22 @@ def fetch_and_print_posts():
             print(post["title"])
 
 def fetch_and_save_posts():
-    res = request.get('https://jsonplaceholder.typicode.com/posts')
+    res = requests.get('https://jsonplaceholder.typicode.com/posts')
 
     print(f"Status Code: {res.status_code}")
 
     if res.status_code == 200:
         posts = res.json()
+
+        structured_data = []
+        for post in posts:
+            structured_data.append({
+                "id": post["id"],
+                "title": post["title"],
+                "body": post["body"]
+            })
+
+        with open("posts.csv", mode='w', newline="", encoding="utf-8") as file:
+            writer = csv.DictWriter(file, fieldnames=["id", "title", "body"])
+            writer.writeheader()
+            writer.writerows(structured_data)
