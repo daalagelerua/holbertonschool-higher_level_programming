fd2955441ca59e2bb9b3e42dd34948409c93d25b
daalagelerua <aurele284@gmail.com>
2025-02-20 16:42:50 +0100

    flask api

---
+++ restful-api/task_04_flask.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+from flask import Flask, request
+from flask import jsonify
+from markupsafe import escape
+
+
+users = {"jane": {"name": "Jane", "age": 28, "city": "Los Angeles"}}
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return ("Welcome to the Flask API!")
+            
+@app.route('/data')
+def data():
+    return jsonify(users)
+
+@app.route('/status')
+def status():
+    return "OK"
+
+@app.route("/add_user", methods=['POST'])
+def add_user():
+    data = request.get_json()
+    if not data or 'username' not in data:
+        return jsonify({"error": "Username is required"}), 400
+    new_user = {
+        data.get('username'): {
+            key: value for key, value in data.items() if key != "username"}}
+    users.update(new_user)
+    return jsonify(new_user), 201
+
+@app.route('/users/<username>')
+def get_user(username):
+    user_data = users.get(escape(username))
+    if user_data:  
+        return jsonify({"username": username, **user_data})  
+    return jsonify({"error": "User not found"}), 404
+
+
+if __name__ == "__main__": app.run()
