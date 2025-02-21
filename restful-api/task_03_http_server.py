55987b231a9caa88d2783ea05746f7141c753aad
daalagelerua <aurele284@gmail.com>
2025-02-21 11:05:53 +0100

    http server

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -8,7 +8,7 @@ class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == "/":
             self.send_response(200)
-            self.send_header("Content-Type", "text/plain")
+            self.send_header("Content-Type", "text/html")
             self.end_headers()
             self.wfile.write(b"Hello, this is a simple API!")
 
@@ -21,27 +21,29 @@ class handler(BaseHTTPRequestHandler):
                 "age": 30,
                 "city": "New York"
             }
-            self.wfile.write(json.dumps(dataset).encode())
+            self.wfile.write(json.dumps(dataset).encode("utf-8"))
 
         elif self.path == "/status":
             self.send_response(200)
-            self.send_header("Content-Type", "application/json")
+            self.send_header("Content-Type", "text/html")
             self.end_headers()
-            status_data = {"status": "OK"}
-            self.wfile.write(json.dumps(status_data).encode())
+            self.wfile.write(b"OK")
 
         elif self.path == "/info":
             self.send_response(200)
             self.send_header("Content-Type", "application/json")
             self.end_headers()
 
-            info = {"version": "1.0", "description": "A simple API built with http.server"}
+            info = {"version": "1.0", "description":
+                    "A simple API built with http.server"}
             self.wfile.write(json.dumps(info).encode())
 
         else:
             self.send_response(404)
+            self.send_header("Content-type", "text/html")
             self.end_headers()
             self.wfile.write(b"Endpoint not found")
+
 
 server = HTTPServer(("localhost", 8000), handler)
 server.serve_forever()
