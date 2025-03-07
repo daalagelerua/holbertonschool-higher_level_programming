4ca263a32e208233444a7d60c3af2b7bf14e5c63
daalagelerua <aurele284@gmail.com>
2025-03-07 15:54:43 +0100

    bullshit for checker

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -11,21 +11,25 @@ import sys  # To retrieve the arguments passed to the script
 if __name__ == "__main__":
     """No execution when imported"""
 
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
     # Connect to MySQL server
     db = MySQLdb.connect(
         host="localhost",  # MySQL server adress (local here)
         port=3306,  # MySQL default port
         # Parameters
-        user=sys.argv[1],   # User name
-        passwd=sys.argv[2],  # Password
-        db =sys.argv[3]       # Database name
+        user=username,
+        passwd=password,
+        db=db_name
     )
 
 # Create a cursor object to execute sql requests and retrieve the results
 cursor = db.cursor()
 
 # Execute the query to get the state given as argument
-state_name = sys.argv[4]  # Name of state will be the fourth argument
 query = (
     "SELECT * FROM states WHERE name LIKE BINARY = '{}'"
     "ORDER BY id ASC".format(state_name)
