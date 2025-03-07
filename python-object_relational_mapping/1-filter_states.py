83d75e1cdfc9e96110451bdc87f39fb4030ed5c5
daalagelerua <aurele284@gmail.com>
2025-03-07 16:04:14 +0100

    bullshit for checker

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -2,22 +2,28 @@
 """
 This script lists all states with a name starting with 'N' (upper N)
 from the database hbtn_0e_0_usa
+Takes 3 arguments: mysql username, mysql password and database name.
 """
 
 import MySQLdb  # To communicate with MySQL
+import sys
 
 
 if __name__ == "__main__":
     """No execution when imported"""
 
+    user=sys.argv[1],   # User name
+    passwd=sys.argv[2],  # Password
+    db=sys.argv[3]       # Database name
+    
     # Connect to MySQL server
     db = MySQLdb.connect(
         host="localhost",  # MySQL server adress (local here)
         port=3306,  # MySQL default port
         # Parameters
-        user=sys.argv[1],   # User name
-        passwd=sys.argv[2],  # Password
-        db=sys.argv[3]       # Database name
+        user=username,
+        passwd=password,
+        db=db_name
     )
 
 # Create a cursor object to execute sql requests and retrieve the results
